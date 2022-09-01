8d047d107cf7b809bfd15a6796bc435380609ca4
lesa siroy <lesa@kodego.ph>
2022-09-01 20:55:12 +0800

    first commit

---
+++ src/main/kotlin/FirstKotlinProgram.kt
@@ -0,0 +1,10 @@
+fun main(){
+    //Hello I am KodeGo MD1P Batch
+    /*
+       Author: Lesa
+       Date Created: 9/1/2022
+       Filename: FirstKotlinProgram.kt
+     */
+    println("Lesa Siroy!")
+
+}
