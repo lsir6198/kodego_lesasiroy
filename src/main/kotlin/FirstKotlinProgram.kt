ae857ab98204c6e583d45acdb90eae8fedfbb4cd
lesa siroy <lesa@kodego.ph>
2022-09-02 21:02:06 +0800

    fifth Commit

--- src/main/kotlin/FirstKotlinProgram.kt
+++ src/main/kotlin/FirstKotlinProgram.kt
@@ -5,10 +5,33 @@ fun main(){
        Date Created: 9/1/2022
        Filename: FirstKotlinProgram.kt
      */
+    var firstName :String = "LyanKeem"
+    var age :Int = 3
+    var grade :Double = 90.00
+    var section :Char = 'A'
+    var isEnrolled :Boolean = true
+    println("My son Name is $firstName")
+    println("His age is $age")
+    println("His grade is $grade")
+    println("Section $section")
+    println("Enrolled $isEnrolled")
     println("Lesa Siroy!")
     print("MD1P Pioneering Batch")
     print(" August 2022")
     print("Hello World!!!!")
     println("Hello World1!!!")
-    print("Hello World2!!!")
+    println("Hello World2!!!")
+    print("Enter your name:")
+    var name : String = readln()
+
+    print("My Name is $name ")
+    println("Enter number 1:")
+    var number1 : Int = readln().toInt()
+
+    println("Enter number 2:")
+    var number2 : Int = readln().toInt()
+
+    var sum : Int = number1 + number2
+    print("My number is $sum")
+
 }
