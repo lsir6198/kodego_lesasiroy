a34b0e5c0cbdc9a57bfa7d4c4444ff28dafec1d9
lesa siroy <lesa@kodego.ph>
2022-09-03 09:52:16 +0800

    first commit

---
+++ src/main/kotlin/KotlinProgram2.kt
@@ -0,0 +1,25 @@
+fun main(){
+    var string1 : String = "this is my String"
+    var string2 : String = "this is my String too!"
+
+    //string character
+    println(string1[11])
+    //string length
+    println(string1.length)
+    println(string2.length)
+    //string equality
+    println(string1 == string2)
+    //string uppercase/lowercase
+    println(string1.uppercase())
+    println(string2.lowercase())
+    //startswith
+    println(string2.startsWith( prefix = "this"))
+    //endsswith
+    println(string2.endsWith(suffix = "too!"))
+    //concatenate
+    println(string1+string2)
+    //substring
+    println(string1.substring(6,10))
+    //replace
+
+}
