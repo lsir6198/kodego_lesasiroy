36899fcf6b3db0f78cd7f6a18ac40b0dcc0d9b7a
lesa siroy <lesa@kodego.ph>
2022-09-03 15:04:27 +0800

    first commit

---
+++ src/main/kotlin/KotlinProgram4.kt
@@ -0,0 +1,20 @@
+fun main(){
+    var number : Int = 10
+
+    if(number <10 ) {
+        println("Hello")
+    }
+
+    println("Line ends")
+    var grade : Int = 70
+    if(grade >=75 ){
+        println("you passed!")
+    }
+
+    //range
+    var idNumber : Int = 1
+    if((idNumber >=1)&&(idNumber <=3) ) {
+        println("valid id number")
+        println("Success!!!")
+    }
+}
