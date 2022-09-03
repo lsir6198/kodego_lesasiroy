60296a070bb3fc9c23d390be6ba9586b6e63817a
lesa siroy <lesa@kodego.ph>
2022-09-03 15:35:30 +0800

    second commit

--- src/main/kotlin/KotlinProgram4.kt
+++ src/main/kotlin/KotlinProgram4.kt
@@ -1,20 +1,35 @@
 fun main(){
-    var number : Int = 9
+    var number: Int = 9
 
-    if(number <10 ) {
+    if (number < 10) {
         println("Hello")
         println("Line ends")
     }
 
-    var grade : Int = 80
-    if(grade >=75 ){
-        println("you passed!")
-    }
+    var grade : Int = 100
 
+      if ((grade >= 75) && (grade <= 100)) {
+        println("1.75")
+    } else if ((grade >= 76) && (grade <= 85)) {
+        println("5.0")
+    } else {
+        println("Invalid grade!")
+    }
     //range
     var idNumber : Int = 1
-    if((idNumber >=1)&&(idNumber <=3) ) {
+    if((idNumber >=1)&&(idNumber <=3)) {
         println("valid id number")
         println("Success!!!")
     }
+    var gender : String = "X"
+    if(gender == "M"){
+        println("Male")
+
+    }else if(gender == "F"){
+        println("Female")
+    }else{
+        println("Invalid Gender")
+    }
 }
+
+
