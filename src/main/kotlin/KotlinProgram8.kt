9f0b1f5e28173bcdff553935d11677aabb821fe6
lesa siroy <lesa@kodego.ph>
2022-09-05 19:39:03 +0800

    first commit

---
+++ src/main/kotlin/KotlinProgram8.kt
@@ -0,0 +1,18 @@
+fun main(){
+    //while loop
+    //init
+    var ctr : Int = 1
+    ctr++
+    println(ctr)
+
+    ctr--
+    println(ctr)
+    //condition
+    while(ctr <=5 ){
+        println("Hello World! $ctr")
+        //update
+        ctr++
+    }
+
+
+}
