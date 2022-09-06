72c42992bfa844045bbfa8038f6a0f29fd9a46b9
lesa siroy <lesa@kodego.ph>
2022-09-06 23:54:30 +0800

    1st commit

---
+++ src/main/kotlin/Exercises/Activity01D.kt
@@ -0,0 +1,31 @@
+fun main(){
+    var answer : Boolean = false
+    var ctr = 1
+
+    print("Enter Amount:")
+    var amount : Double = readln().toDouble()
+
+    println("Do you want to add another amount?")
+    var input = readln()
+
+    while(answer){
+        println("Enter another amount:")
+        var amount1: Double = readln().toDouble()
+        ++ctr
+        if (ctr > 5) break
+    }
+
+        print("Divide the value by how many?")
+        var input1: Int = readln().toInt()
+        var quotient = amount / ctr
+        print("Divided value is $quotient")
+
+
+}
+
+
+
+
+
+
+
