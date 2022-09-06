d3d144efd5ff71462e3a631264a210ab503faf4e
lesa siroy <lesa@kodego.ph>
2022-09-07 00:52:21 +0800

    1st commit

---
+++ src/main/kotlin/Exercises/Activity01E.kt
@@ -0,0 +1,24 @@
+fun main (){
+
+//Create an application that will accept 2 string inputs.
+// Your application will print all unique characters in both Strings.
+
+    var name : String = " Lesa "
+    var occupation : String = " BSCS "
+
+    print("Enter your Name:")
+    var input = readln()
+    println(name[1])
+    println(name[2])
+    println(name[3])
+    println(name[4])
+
+    print("Enter your Occupation:")
+    var input2 = readln()
+    println(occupation[1])
+    println(occupation[2])
+    println(occupation[3])
+    println(occupation[4])
+
+}
+
