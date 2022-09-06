3ebded0e22566274471e19493f31d8a89b3896e6
lesa siroy <lesa@kodego.ph>
2022-09-07 01:39:32 +0800

    first Commit

---
+++ src/main/kotlin/Exercises/Activity01F.kt
@@ -0,0 +1,15 @@
+fun main(){
+// Create an application that will accept 1 string.
+// Your application will print “Palindrome” if the string is a palindrome.
+
+    var string1 = " Radar "
+
+    println("First word is$string1")
+    print("String Length is ")
+    println(string1.length)
+    print("Second word is")
+    println(string1.reversed())
+    print("String1 is Palindrome")
+
+
+}
