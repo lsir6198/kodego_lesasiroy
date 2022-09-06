36a7b2e0cc0ac0386e391f142135cd14cfaf9e6b
lesa siroy <lesa@kodego.ph>
2022-09-06 19:34:16 +0800

    1st commit

---
+++ src/main/kotlin/KotlinProgram12.kt
@@ -0,0 +1,16 @@
+fun main(){
+    var myString: String? = "Hello Earth"
+  //  println(myString)
+
+    //?.let{}
+ //       var count = myString?.let{2+2}
+ //       println(count)
+
+    //elvis operator ?:
+ //   val count2 = myString?.length ?: -1
+ //   println(count2)
+
+    //!!
+    val count3 = myString!!.length
+    println(count3)
+}
