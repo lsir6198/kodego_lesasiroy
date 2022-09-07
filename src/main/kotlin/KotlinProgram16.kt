8f58e1a1d367d16495ac4f401611ff86e730d7b9
lesa siroy <lesa@kodego.ph>
2022-09-07 20:45:09 +0800

    1st commit

---
+++ src/main/kotlin/KotlinProgram16.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var names = ArrayList<String>()
+
+    //add elements
+    names.add("John")
+    names.add("Peter")
+    println(names[0])
+    println(names[1])
+    //remove elements
+    names.removeAt(0) //Peter -> index 0
+    //display names[0]
+    println(names[0])
+
+}
+ //array list
