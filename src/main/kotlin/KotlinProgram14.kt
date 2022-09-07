cc6a7da856fc12b23c8677f840e82ac1cdb6b276
lesa siroy <lesa@kodego.ph>
2022-09-07 20:15:06 +0800

    first commit

---
+++ src/main/kotlin/KotlinProgram14.kt
@@ -0,0 +1,10 @@
+fun main(){
+
+    var names = arrayOf<String>("John", "Paul", "Mark", "Ringo", "George")
+    var ctr : Int = 0
+    while (ctr < names.size){
+        println(names[ctr])
+        ctr++
+    }
+
+}
