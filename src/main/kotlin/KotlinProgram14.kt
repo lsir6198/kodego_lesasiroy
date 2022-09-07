674dc7474b0c8d28c3cb2365e579a1e6265a5aa8
lesa siroy <lesa@kodego.ph>
2022-09-08 01:55:49 +0800

    another commit

--- src/main/kotlin/KotlinProgram14.kt
+++ src/main/kotlin/KotlinProgram14.kt
@@ -1,10 +1,20 @@
-fun main(){
+fun main() {
 
     var names = arrayOf<String>("John", "Paul", "Mark", "Ringo", "George")
-    var ctr : Int = 0
-    while (ctr < names.size){
+    var ctr: Int = 0
+    while (ctr < names.size) {
         println(names[ctr])
         ctr++
     }
+    //while(ctr < names.size){
+    // println("Enter new value for indes $ctr")
+    // names[ctr] = readln()
+    // ctr++ }
+    //display new array values
+    // var ctr2 : Int = 0
+    //while(ctr < names.size){
+    //println(names[ctr2])
+    // ctr22++
 
+//    }
 }
