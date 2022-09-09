980f0fceb0c753d9f4a9f10d55377fc2dc7382ab
lesa siroy <lesa@kodego.ph>
2022-09-09 16:16:52 +0800

    another new  commit

--- src/main/kotlin/Exercises/Activity01A.kt
+++ src/main/kotlin/Exercises/Activity01A.kt
@@ -8,25 +8,44 @@ fun main(){
     var name1 = "Lesa Siroy"
     var name2 = "Marc Quinn"
     var name3 = "Lyan Keem"
-    var grade1 :Int = 90
+    var grade1 :Int = 74
     var grade2 :Int = 95
     var grade3 :Int = 93
-    var isEnrolled :Boolean = true
+  //  var isEnrolled :Boolean = true
 
 
     println("Directory of Students in Class MD1P Batch 2")
     println("List of Students:")
     print("$name1, ")
-    println("is Enrolled = $isEnrolled")
-    println("Grade is $grade1, Outstanding/Passed")
-    println("")
+  //  println("is Enrolled = $isEnrolled")
+    println("Grade is $grade1")
+    if (grade1 >=75) {
+        println("Passed")
+        }
+    else {
+        println("Failed")
+         }
+
     print("$name2, ")
-    println("is Enrolled = $isEnrolled")
-    println("Grade is $grade2, Outstanding/Passed")
-    println("")
+  //  println("is Enrolled = $isEnrolled")
+    println("Grade is $grade2")
+    if (grade2 >=75)
+        {
+        println("Passed")
+        }
+    else {
+        println("Failed")
+         }
+
     print("$name3, ")
-    println("is Enrolled = $isEnrolled")
-    println("Grade is $grade3, Outstanding/Passed")
-    println("")
+ //   println("is Enrolled = $isEnrolled")
+    println("Grade is $grade3")
+    if (grade3 >=75)
+        {
+        println("Passed")
+        }
+    else {
+        println("Failed")
+         }
     print("-------End of List-------")
 }
