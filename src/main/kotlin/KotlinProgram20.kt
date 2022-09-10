dd05c820eff65b64b37d8917a42e3a7124e5934c
lesa siroy <lesa@kodego.ph>
2022-09-10 10:57:46 +0800

    first commit

---
+++ src/main/kotlin/KotlinProgram20.kt
@@ -0,0 +1,16 @@
+fun main(){
+    var numbers = mutableListOf<Int>()
+    var ctr : Int = 0
+
+    while(ctr < 5){
+        println("Enter number ${ctr+1}")
+        var input = readln().toInt()
+        numbers.add(input)
+        ctr++
+    }
+    //display list
+    println("Displaying List")
+    for(value in numbers){
+        println(value)
+    }
+}
