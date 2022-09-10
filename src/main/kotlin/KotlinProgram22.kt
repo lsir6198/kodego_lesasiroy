597bb572d99c43caa252e078a34431fae4c45afa
lesa siroy <lesa@kodego.ph>
2022-09-10 13:52:38 +0800

    first commit

---
+++ src/main/kotlin/KotlinProgram22.kt
@@ -0,0 +1,31 @@
+fun main(){
+    displayName()
+    displayMessage("Hello!")
+
+  //  println("Enter number 1:")
+  //  var number : Int = readln().toInt()
+
+  //  println("The cube is ${number * number * number}")
+
+
+}
+//no return no parameter
+fun displayName():Unit {
+    println("Peter")
+}
+//no return with 1 parameter
+fun displayMessage(msg: String) {
+    println("The message is $msg")
+
+    }
+
+//built-in functions/standard functions in kotlin
+//abstraction
+
+//println()
+//toInt()
+//main()
+//arrayof()
+//listof()
+
+//user-defined function
