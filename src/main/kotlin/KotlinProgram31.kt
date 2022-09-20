fad734a7b6e5b01931927a048f257d7d1b02f5b1
lesa siroy <lesa@kodego.ph>
2022-09-20 20:08:46 +0800

    another new commit

---
+++ src/main/kotlin/KotlinProgram31.kt
@@ -0,0 +1,17 @@
+fun main(){
+    try {
+        println(":Enter number 1:")
+        var num1 : Int = readln().toInt()
+    }catch (e:Exception){
+        println("Invalid input! Digits only!")
+    }
+
+    println("End of code!!!")
+}
+
+//Exception - something that disrupts a normal flow of your code.
+//recoverable - you handle exceptions
+
+//Error - unrecoverable.
+
+//try - catch block = two blocks of code(catch the code that might throw an exception.
