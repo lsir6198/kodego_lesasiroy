3a669934838bcaca70071ff3d074be27d52fd1b9
lesa siroy <lesa@kodego.ph>
2022-09-24 10:27:00 +0800

    another new commit

--- src/main/kotlin/com/oop/Person.kt
+++ src/main/kotlin/com/oop/Person.kt
@@ -1,9 +1,23 @@
 package com.oop
 
+//Abstraction = abstract/hidden
+//encapsulation = cannot directly access the variables
+
 class Person {
     var name : String = "John"
+        get() {return field}
+        set(value) {
+            field = value
+        }
     var age : Int = 23
-
+        get() {return field}
+        set(value) {
+            if (value < 1){
+                println("Invalid Age")
+            }else{
+                field = value
+            }
+        }
     init {
         println("Object Created!")
         println("name is $name, age is $age")
