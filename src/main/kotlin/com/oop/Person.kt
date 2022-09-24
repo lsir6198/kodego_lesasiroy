47654b143c411a0d9c4e375c1e9bad1642595ed9
lesa siroy <lesa@kodego.ph>
2022-09-24 10:08:58 +0800

    new commit

--- src/main/kotlin/com/oop/Person.kt
+++ src/main/kotlin/com/oop/Person.kt
@@ -1,4 +1,20 @@
 package com.oop
 
 class Person {
+    var name : String = "John"
+    var age : Int = 23
+
+    init {
+        println("Object Created!")
+        println("name is $name, age is $age")
+    }
+
+    fun upAge():Int{
+        age++
+        return age
+    }
+
+    fun change(age:Int){
+        println(age)
+    }
 }
