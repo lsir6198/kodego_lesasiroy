ff5413965c62e18fa2796ff1706ee7c6f68e6f00
lesa siroy <lesa@kodego.ph>
2022-09-24 10:34:02 +0800

    another new commit

--- src/main/kotlin/com/oop/Main.kt
+++ src/main/kotlin/com/oop/Main.kt
@@ -1,7 +1,10 @@
 package com.oop
 
 fun main(){
-    var p1 = Person()
-    var ageFromPerson = p1.upAge()
+    var joePerson = Person()
+    joePerson.name = "Peter"
+    println(joePerson.name)
+    joePerson.age = -1
+    var ageFromPerson = joePerson.upAge()
     println("Hello World!")
 }
