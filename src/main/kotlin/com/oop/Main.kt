9caa7ea13ef1370c91b123e992514d686fb13e69
lesa siroy <lesa@kodego.ph>
2022-09-27 21:42:02 +0800

    another new commit

--- src/main/kotlin/com/oop/Main.kt
+++ src/main/kotlin/com/oop/Main.kt
@@ -1,15 +1,14 @@
 package com.oop
 
 fun main(){
-/*
-    var joePerson = Person()
-    joePerson.name = "Peter"
-    println(joePerson.name)
-    joePerson.age = -1
-    var ageFromPerson = joePerson.upAge()
-*/
-    var joeStudent = Student(6199)
-    println(joeStudent.name)
-    joeStudent.age = -1
-    println("Hello World!")
+    var manager = Manager()
+    manager.computeBonus()
+    //1000.00 + 10%
+
+    var staff = Staff()
+    staff.computeBonus()
+    //1000
+
+    println("Staff salary ${staff.salary}")
+    println("Manager salary ${manager.salary}")
 }
