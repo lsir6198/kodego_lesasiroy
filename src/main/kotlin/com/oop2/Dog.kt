dc26b5a35040bb6424af0588e3ea4c2d91487664
lesa siroy <lesa@kodego.ph>
2022-09-28 20:10:54 +0800

    new commit

--- src/main/kotlin/com/oop2/Dog.kt
+++ src/main/kotlin/com/oop2/Dog.kt
@@ -1,4 +1,11 @@
 package com.oop2
 
-class Dog {
+class Dog: Animal() {
+    override fun makeSound() {
+        println("Aw Aw Aw")
+    }
+
+    override fun walk() {
+        println("A dog walks on four legs")
+    }
 }
