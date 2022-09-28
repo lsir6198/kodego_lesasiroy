72280c0e045886d5b353de2c9eac50cc97aaedf1
lesa siroy <lesa@kodego.ph>
2022-09-28 20:10:03 +0800

    new commit

---
+++ src/main/kotlin/com/oop2/Animal.kt
@@ -0,0 +1,14 @@
+package com.oop2
+
+abstract class Animal {
+
+        abstract fun walk()
+        abstract fun makeSound()
+
+}
+
+    //it can have abstract functions => are functions without a body/implementation
+    //only abstract classes can have abstract function
+    //cannot create an object from an abstract class
+    //abstract is always a superclass only
+    //when you inherit from an abstract class, you are required to override all its abstract functions
