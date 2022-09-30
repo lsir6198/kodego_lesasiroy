5de42a725797e12100e89a40762def250a594af7
lesa siroy <lesa@kodego.ph>
2022-09-30 20:14:27 +0800

    new commit

--- src/main/kotlin/com/oop3/Animal.kt
+++ src/main/kotlin/com/oop3/Animal.kt
@@ -1,4 +1,11 @@
 package com.oop3
 
+// not a class, no constructors
+//cannot create an object
+//can have abstract functions without abstract keyword
+//being implemented rather than inherited
+
 interface Animal {
+    fun walk()
+    fun makeSound()
 }
