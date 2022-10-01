ad819b8c56ba1cd147eb35af8fc2a73815fdb119
lesa siroy <lesa@kodego.ph>
2022-10-01 13:52:41 +0800

    another new commit

--- src/main/kotlin/Exercises/Activity03A01F.kt
+++ src/main/kotlin/Exercises/Activity03A01F.kt
@@ -7,7 +7,11 @@ fun main(){
     print("Enter a word: ")
     var word = readln().lowercase()
 
-    try {word}catch (e:CharacterCodingException){
+    try {
+        if (word.isEmpty()){
+            throw Exception()
+        }
+    }catch (e:Exception){
         println(e)
         println("Invalid Input")
     }
@@ -15,8 +19,13 @@ fun main(){
     print("Next step is to check if word is Palindrome, Do you want to proceed? ")
     var answer = readln().lowercase()
 
-    try {answer}catch (e:CharacterCodingException){
+    try {
+        if (answer.isEmpty()){
+            throw Exception()
+        }
+    }catch (e:Exception){
         println(e)
+        println("Invalid Input")
     }
 
     if (answer == "yes")
